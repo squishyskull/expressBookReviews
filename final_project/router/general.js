d35f1d747674a1b8e39857b7ae18a90148262df8
squishyskull <tamara.e.brooks1@gmail.com>
2023-02-21 06:28:34 -0500

    justincase

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,22 +10,27 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
+
+
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn])
  });
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    res.send(JSON.stringify({author}, null, 4));
 });
 
 // Get all books based on title
