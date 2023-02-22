a627cd590dfdcdd10e5b98249c0914c25015c42b
squishyskull <tamara.e.brooks1@gmail.com>
2023-02-21 23:00:33 -0500

    promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -88,3 +88,67 @@ public_users.get('/review/:isbn',function (req, res) {
   res.send(JSON.stringify(book["reviews"], null, 4));
 });
 module.exports.general = public_users;
+
+// TASK 10 - Get the book list available in the shop using promises
+public_users.get('/books',function (req, res) {
+
+    const get_books = new Promise((resolve, reject) => {
+        resolve(res.send(JSON.stringify({books}, null, 4)));
+      });
+
+      get_books.then(() => console.log("Promise for Task 10 resolved"));
+
+  });
+
+// TASK 11 - Get the book list based on ISBN
+public_users.get('/books/:isbn',function (req, res) {
+
+    const get_isbn = new Promise((resolve, reject) => {
+        const isbn = req.params.isbn;
+        resolve(res.send(books[isbn]));
+      });
+
+      get_isbn.then(() => console.log("Promise for Task 11 resolved"));
+  });
+
+// TASK 12 - Get the book list based on author
+public_users.get('/authr/:author',function (req, res) {
+
+    const get_auths = new Promise((resolve, reject) => {
+        const auth = req.params.author;
+        let result = [];
+  
+        //collect keys of books
+        for ( let key in books ) {
+            let book = books[key];
+            if (book["author"] == auth) {
+                result.push(book);
+             }
+        };
+        resolve(res.send(JSON.stringify(result, null, 4)));
+    });
+  get_auths.then(() => console.log("Promise for Task 12 resolved"));
+});
+
+
+// TASK 13 - Get the book list based on title
+public_users.get('/titl/:title',function (req, res) {
+
+    const get_auths = new Promise((resolve, reject) => {
+        const titl = req.params.title;
+        let result = [];
+  
+        //collect keys of books
+        for ( let key in books ) {
+            let book = books[key];
+            if (book["title"] == titl) {
+                result.push(book);
+             }
+        };
+        resolve(res.send(JSON.stringify(result, null, 4)));
+    });
+  get_auths.then(() => console.log("Promise for Task 13 resolved"));
+});
+
+
+module.exports.general = public_users;
